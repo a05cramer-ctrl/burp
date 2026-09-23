2d57da6a314596768eae17d4b593fc58174a045c
a05cramer-ctrl <a05cramer@gmail.com>
2026-09-23 16:37:28 +0200

    Add CA + X link

--- config.js
+++ config.js
@@ -2,11 +2,11 @@
 window.BURP_CFG = {
   NAME:   "BurpCoin",
   TICKER: "BURP",
-  CA:     "",
+  CA:     "2s6MSbTDRTPVRB1yYVPSb1zd7WQP5UeQDsoJxaH2PnNJ",
   CHAIN:  "solana",
   PAD:    "stonkfun",      // stonkfun | pumpfun
   PAIR:   "KO",            // quote token, shown as KOx
-  X:      "",
+  X:      "https://x.com/burpcoinfun",
   BUY:    "",              // optional override
   CHART:  ""               // optional override
 };
